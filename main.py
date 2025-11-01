c32bf381980c1156dec21224cab0a6420ffa027d
Rahul <134809022+rahulrj95@users.noreply.github.com>
2025-11-01 12:52:52 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello from GitHub Build!")
+
+if _name_ == "_main_":
+    MyApp().run()
